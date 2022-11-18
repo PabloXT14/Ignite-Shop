e35ce50f84f4112967355e01ef2420dd92edbae6
Pablo Alan <pabloxt14@gmail.com>
2022-11-18 09:04:44 -0300

    feat: configureted _document.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <h1>
+      <Head>
+        <title>Ignite Shop</title>
+      </Head>
       Hello world
     </h1>
   )
