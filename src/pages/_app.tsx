241cf0a2d907bab2e162d8e7b1739eb1068a7d72
Pablo Alan <pabloxt14@gmail.com>
2022-11-21 23:04:47 -0300

    feat: create header styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { globalStyles } from "../styles/global"
+import Image from 'next/image' 
+import logoImg from '../assets/logo-ignite-shop.svg'
+import * as S from '../styles/pages/app'
 
 globalStyles();// recomendado colocar aqui ao invés do dentro do App, pois a cada alteração que fizer com que o componente App seja recarregado todo o globalStyles seria recarrergado, o que é ruim para performance.
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <S.AppContainer>
+      <S.Header>
+        <Image src={logoImg} alt="" quality={100} />
+      </S.Header>
+      <Component {...pageProps} />
+    </S.AppContainer>
+  )
 }
