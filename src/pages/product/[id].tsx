dc0562d8c337b0c705a21dab67f6d22ac47f97be
Pablo Alan <pabloxt14@gmail.com>
2022-11-30 17:00:49 -0300

    feat: estructuring product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
 import { useRouter } from 'next/router'
 
+import * as S from '../../styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>{JSON.stringify(query)}</h1>
+    <S.ProductContainer>
+      <S.ImageContainer>
+
+      </S.ImageContainer>
+
+      <S.ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt voluptate iste officiis voluptates inventore blanditiis in doloribus maxime quo explicabo suscipit et nostrum earum veniam eum laborum consequatur, impedit fugit!</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </S.ProductDetails>
+    </S.ProductContainer>
   )
 }
