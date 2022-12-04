ce7b3d0d62c8d30757ecffa93dad34ead100163b
Pablo Alan <pabloxt14@gmail.com>
2022-12-04 14:31:08 -0300

    feat: prefetch from Links on Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home({ products }: HomeProps) {
 
       {products.map(product => {
         return (
-          <Link key={product.id} href={`/product/${product.id}`}>
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
             <S.Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
 
